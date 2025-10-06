a9d8ee6323d0ae4d375e3c0912bb21312aa45cec
Simeon Akinrinola <simeonakinrinola7@gmail.com>
2025-10-06 09:42:09 +0100

    monday `6th Oct`

--- html/javascript/js_practice7/js_codealong7.js
+++ html/javascript/js_practice7/js_codealong7.js
@@ -1,12 +1,20 @@
+// 
 class Book {
     constructor (title, author, year) {
         this.title = title
         this.author = author
         this.year = year
+        this.isBorrowed = false
+
     }
 
     getSummary() {
         return `Book: ${this.title} by ${this.author}(${this.year})`
+    }
+
+    borrowBook() {
+        this.isBorrowed = true
+        return (`${this.title} is borrowed: ${this.isBorrowed}`)
     }
 }
 
@@ -14,4 +22,4 @@ let number1 = new Book("Please don't kill a mockingbird", "Inioluwa", 2025)
 let number2 = new Book("Attendance in 10 minutes", "Ngozi", 2025)
 
 let display = document.getElementById("display")
-display.innerHTML = `${number1.getSummary()} <br> ${number2.getSummary()}`
+display.innerHTML = `${number1.getSummary()} <br> ${number2.getSummary()} <br> ${number1.borrowBook()}`
